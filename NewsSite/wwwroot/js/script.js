0dbfbde5d4c8c3aa712c0896470ced5ba3c05b2a
Mauricio Ludwig <mrdamiengallagher@gmail.com>
2017-12-20 21:15:01 +0100

    HTML boilerplate added

--- NewsSite/wwwroot/js/script.js
+++ NewsSite/wwwroot/js/script.js
@@ -1,5 +1,40 @@
 ﻿$(document).ready(function () {
 
+    // Cached variables
+    var recreateUsers = $('#recreateUsers');
+    var getUsers = $('#getUsers');
 
+    var userEmailList = $('#userEmailList');
+    var signIn = $('#signIn');
+
+    var accessToOpenArticle = $('#accessToOpenArticle');
+    var accessToHiddenArticle = $('#accessToHiddenArticle');
+    var accessToHiddenArticleAndEG20 = $('#accessToHiddenArticleAndEG20');
+    var allowedToPublishSportsArticle = $('#allowedToPublishSportsArticle');
+    var allowedToPublishCultureArticle = $('#allowedToPublishCultureArticle');
+
+    recreateUsers.on('click', function () {
+    });
+
+    getUsers.on('click', function () {
+    });
+
+    signIn.on('click', function () {
+    });
+
+    accessToOpenArticle.on('click', function () {
+    });
+
+    accessToHiddenArticle.on('click', function () {
+    });
+
+    accessToHiddenArticleAndEG20.on('click', function () {
+    });
+
+    allowedToPublishSportsArticle.on('click', function () {
+    });
+
+    allowedToPublishCultureArticle.on('click', function () {
+    });
 
 });
